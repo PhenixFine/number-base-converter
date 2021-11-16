e6f4808db6d7301af3402adccd43c861a8d6f906
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-11-15 23:51:14 -0500

    Stage 4 of 4 for JetBrains Academy - Kotlin - Number Base Converter project. For this stage, the rounding and scales we should use while converting from one base to the next wasn't defined ( other than the final result should be five digits ), so the three scales I used seemed to be the best for passing their test ( like simply starting out with rounding the first base conversion to five digits would fail the tests, so I had to drop the scale in stages, starting with 10, then to 7, and ending with 5, if it was converting two bases that weren't of base 10 ).
    Main.kt - changes:
    • baseToBase() - was adjusted to not check if two bases were equal ( in this stage, doing that would fail the tests ). I considered just returning the given value in such cases, but figured maybe rounding was needed. I also moved the checking of a negative number it to help simplify things. The call to getConversion() was adjusted to call getResult() instead.
    • getConversion() - was renamed getWholeNumber()
    • decimalToBase() - renamed to bigIntToBase and adjusted to no longer handle negative numbers.
    • baseToDecimal() - renamed baseToBigInt() and adjusted to no longer handle negative numbers. The checking of if the number was valid was moved to isValid() and the setting of the number to lowercase was moved to getString()
    • getString() - adjusted to set given line from user to lowercase
    • getResult() - added to check and see if given number is a fraction, and if so it calls getFraction() and if not it calls getWholeNumber() returning the result of either.
    • getFraction() - handles converting a fraction from one base to the next, calling baseToBigDecimal() and fractionToBase() when appropriate.
    • bigDecimalToBase() - handles converting a BigDecimal to the required base. Converting the fractional part itself, and using bigIntToBase() for converting the whole number.
    • baseToBigDecimal() - handles converting a given base number and converting it to BigDecimal, using baseToBigInt() for converting the whole number and converting the fraction part itself.
    • isValid() - verifies a given string isn't empty and is composed of letters and digits only.
    
    README.md - changes:
    • updated to the info for Stage 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
+import java.math.BigDecimal
 import java.math.BigInteger
+import java.math.RoundingMode.FLOOR
+import java.math.RoundingMode.HALF_UP
 
 val BASES = 2..36
 
@@ -15,25 +18,22 @@ fun main() {
 }
 
 fun baseToBase(stringBases: List<String>) {
-    if (stringBases.size == 2 && stringBases[0] != stringBases[1]) {
+    if (stringBases.size == 2) {
         val source = getBase(stringBases[0]) ?: return
         val target = getBase(stringBases[1]) ?: return
         val message = "Enter number in base $source to convert to base $target (To go back type /back) "
         var command = getString(message)
 
         while (command != "/back") {
-            val result = getConversion(source, target, command)
+            val negative = command.startsWith('-')
+            val number = if (negative) command.substring(1) else command
+            var result = getResult(source, target, number)
+
+            if (negative && result != null) result = "-$result"
             println(if (result != null) "Conversion result: $result" else "$command is invalid for base $source")
             command = getString("\n" + message)
         }
-    } else println("Please enter two numbers within ($BASES), that are not equal to each other")
-}
-
-fun getConversion(source: BigInteger, target: BigInteger, number: String): String? {
-    val decimal = (if (source == BigInteger.TEN) number.toBigIntegerOrNull() else
-        (baseToDecimal(number, source))) ?: return null
-
-    return if (target == BigInteger.TEN) decimal.toString() else decimalToBase(decimal, target)
+    } else println("Please enter two numbers within ($BASES)")
 }
 
 fun getBase(number: String): BigInteger? {
@@ -45,36 +45,82 @@ fun getBase(number: String): BigInteger? {
     } else base.toBigInteger()
 }
 
-fun decimalToBase(number: BigInteger, base: BigInteger): String {
+fun getResult(source: BigInteger, target: BigInteger, number: String): String? {
+    return if (number.count { it == '.' } == 1) {
+        getFraction(source.toBigDecimal(), target.toBigDecimal(), number)
+    } else getWholeNumber(source, target, number)
+}
+
+fun getWholeNumber(source: BigInteger, target: BigInteger, number: String): String? {
+    val decimal = (if (source == BigInteger.TEN) number.toBigIntegerOrNull() else
+        (baseToBigInt(number, source))) ?: return null
+
+    return if (target == BigInteger.TEN) decimal.toString() else bigIntToBase(decimal, target)
+}
+
+fun getFraction(source: BigDecimal, target: BigDecimal, number: String): String? {
+    val decimal = (if (source.compareTo(BigDecimal.TEN) == 0) number.toBigDecimalOrNull() else
+        baseToBigDecimal(number, source)) ?: return null
+
+    return if (target.compareTo(BigDecimal.TEN) == 0) decimal.setScale(5, HALF_UP).toString()
+    else bigDecimalToBase(decimal.setScale(7, HALF_UP), target.toInt())
+}
+
+fun bigIntToBase(number: BigInteger, base: BigInteger): String {
     var result = ""
-    var quotient = number.abs()
+    var quotient = number
 
     while (quotient >= base) {
         result = getBaseConversion(quotient.remainder(base).toInt()) + result
         quotient /= base
     }
-    return (if (number < BigInteger.ZERO) "-" else "") + getBaseConversion(quotient.toInt()) + result
+    return getBaseConversion(quotient.toInt()) + result
+}
+
+fun bigDecimalToBase(number: BigDecimal, base: Int): String {
+    var result = ""
+    val wholeNumber = number.setScale(0, FLOOR)
+    var fraction = (number - wholeNumber).toDouble()
+
+    while (fraction > 0 && result.length < 5) {
+        fraction *= base
+        val wholeNum = fraction.toInt()
+        fraction -= wholeNum
+        result += getBaseConversion(wholeNum)
+    }
+    if (result.length < 5) result += "0".repeat(5 - result.length)
+    return bigIntToBase(wholeNumber.toBigInteger(), base.toBigInteger()) + ".$result"
 }
 
 fun getBaseConversion(remainder: Int) = (if (remainder >= 10) 'a' + remainder - 10 else remainder).toString()
 
-fun baseToDecimal(number: String, base: BigInteger): BigInteger? {
-    val negative = number.startsWith("-")
-    val abs = if (negative) number.substring(1) else number
-    val valid = abs.isNotEmpty() && abs.all { it.isLetterOrDigit() }
-    val list = if (valid) abs.reversed().lowercase().toCharArray()
+fun baseToBigInt(number: String, base: BigInteger): BigInteger? {
+    val list = if (isValid(number)) number.reversed().toCharArray()
         .map { getDecimalConversion(it).toBigInteger() } else emptyList()
 
     return if (list.isEmpty() || list.any { it >= base }) null else {
         var multiply = BigInteger.ONE
-        val sum = list.sumOf { it * multiply.also { multiply *= base } }
-        if (negative) -sum else sum
+        list.sumOf { it * multiply.also { multiply *= base } }
     }
 }
+
+fun baseToBigDecimal(number: String, base: BigDecimal): BigDecimal? {
+    val (whole, fraction) = number.split(".").map { it.ifEmpty { "0" } }
+    val wholeNum = (baseToBigInt(whole, base.toBigInteger()) ?: return null).toBigDecimal()
+    val list = if (isValid(fraction)) fraction.toCharArray()
+        .map { getDecimalConversion(it).toBigDecimal() } else emptyList()
+
+    return if (list.isEmpty() || list.any { it >= base }) null else {
+        var divisor = base
+        wholeNum + list.sumOf { it.divide(divisor, 10, HALF_UP).also { divisor *= base } }
+    }
+}
+
+fun isValid(number: String) = number.isNotEmpty() && number.all { it.isLetterOrDigit() }
 
 fun getDecimalConversion(number: Char) = if (number.isLetter()) number.code - 87 else number.digitToInt()
 
 fun getString(message: String): String {
     print(message)
-    return readLine()!!
+    return readLine()!!.lowercase()
 }
